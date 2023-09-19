41d5b82f9596e18f0b1c1b6555a7c11c4e3f3890
EZ586 <ezhang310@gatech.edu>
2023-09-19 15:33:19 -0400

    Added ability to add clusters in an organized way

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,34 +11,55 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 import './App.css'
 
-const initialNodes = [
-  { id: '1', position: { x: 80, y: 80 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 50 }, data: { label: '2' } },
-  { id: '3', position: { x: 0, y: 60 }, data: { label: '3' } },
-  { id: '4', position: { x: 0, y: 100 }, data: { label: '4' } },
+
+// Set number of comments around center
+let numComments = 5;
+// nodes
+let initialNodes = [
+  { id: '1', position: { x: 1, y: 1 }, data: { label: 'Cluster 1' } },
+  { id: '2', position: { x: 1, y: 1 }, data: { label: '2' } },
+  { id: '3', position: { x: 1, y: 1 }, data: { label: '3' } },
+  { id: '4', position: { x: 1, y: 1 }, data: { label: '4' } },
 ];
-const initialEdges = [
+// edges
+let initialEdges = [
   { id: 'e1-2', source: '1', target: '2' },
   { id: 'e1-3', source: '1', target: '3' },
   { id: 'e1-4', source: '1', target: '4' },
 ];
-
-
+// test node
+let initTestNodes = [{ id: '1', position: { x: 1, y: 1 }, data: { label: 'Cluster 1' } }]
+// test edges
+let initTestEdges = []
+// create nodes/edges based on number of comments
+for (let i = 2; i < numComments + 2; i++) {
+  console.log(i);
+  initTestNodes.push({ id: i.toString(), position: { x: 1, y: 1 }, data: { label: i.toString() } })
+  initTestEdges.push({ id: 'e1-'+i.toString(), source: '1', target: i.toString() })
+}
+console.log(initTestNodes)
+//pls work
+initialNodes = initTestNodes;
+initialEdges = initTestEdges;
+// initialize values
+var NodeNum = initialNodes.length - 1;
+var angle = 2 * Math.PI / NodeNum;
+var currAngle = 0;
+var radius = 200;
+// Updated positions
+for (let i = 1; i < NodeNum + 1; i++) {
+  console.log(i);
+  console.log(JSON.stringify(initialNodes[i].position));
+  initialNodes[i].position.x = Math.cos(currAngle) * radius;
+  console.log(Math.cos(currAngle)) 
+  initialNodes[i].position.y = Math.sin(currAngle) * radius;
+  console.log(JSON.stringify(initialNodes[i].position));
+  currAngle = currAngle + angle;
+}
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  useEffect(() => {
-  
-    console.log("What")
-
-    var angle = 360 / 3;
-    for (let i = 0; i < 3; i++) {
-      console.log(i);
-    }
-  
-  }, []);
-  
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
