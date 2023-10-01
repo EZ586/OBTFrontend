cad4e7fa92534a31c7d034f291373c6af09ff592
Sylvia Li <sylvia88668@gmail.com>
2023-10-01 17:16:34 -0400

    added text comments to mind map

--- frontend/src/pages/MindMap/MindMap.jsx
+++ frontend/src/pages/MindMap/MindMap.jsx
@@ -1,49 +1,37 @@
 import React from "react";
-import {
-  ForceGraph2D,
-  ForceGraph3D,
-  ForceGraphVR,
-  ForceGraphAR,
-} from "react-force-graph";
+import { ForceGraph2D } from "react-force-graph";
 import "./MindMap.css";
 
 function genRandomTree(N = 300, reverse = false) {
   return {
-    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
-    links: [...Array(N).keys()]
-      .filter((id) => id)
-      .map((id) => ({
-        [reverse ? "target" : "source"]: id,
-        [reverse ? "source" : "target"]: Math.round(Math.random() * (id - 1)),
-      })),
+    nodes: [
+      {
+        id: "1",
+        name: "Post: I like cheese",
+        val: 10,
+      },
+      {
+        id: "2",
+        name: "Opinion: I hate cheese",
+        val: 10,
+      },
+      {
+        id: "3",
+        name: "Opinion: I like cheese too",
+        val: 10,
+      },
+    ],
+    links: [
+      {
+        source: "1",
+        target: "2",
+      },
+      {
+        source: "1",
+        target: "3",
+      },
+    ],
   };
-}
-
-function nodePaint({ id, x, y }, color, ctx) {
-  ctx.fillStyle = color;
-  [
-    () => {
-      ctx.fillRect(x - 6, y - 4, 12, 8);
-    }, // rectangle
-    () => {
-      ctx.beginPath();
-      ctx.moveTo(x, y - 5);
-      ctx.lineTo(x - 5, y + 5);
-      ctx.lineTo(x + 5, y + 5);
-      ctx.fill();
-    }, // triangle
-    () => {
-      ctx.beginPath();
-      ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
-      ctx.fill();
-    }, // circle
-    () => {
-      ctx.font = "10px Sans-Serif";
-      ctx.textAlign = "center";
-      ctx.textBaseline = "middle";
-      ctx.fillText("Text", x, y);
-    }, // text
-  ][id % 4]();
 }
 
 // gen a number persistent color from around the palette
@@ -60,10 +48,13 @@ const MindMap = () => {
         <ForceGraph2D
           graphData={genRandomTree(20)}
           nodeLabel="id"
-          nodeCanvasObject={(node, ctx) =>
-            nodePaint(node, getColor(node.id), ctx)
-          }
-          nodePointerAreaPaint={nodePaint}
+          nodeCanvasObject={(node, ctx) => {
+            ctx.fillStyle = getColor(node.id);
+            ctx.font = "10px Sans-Serif";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText(node.name, node.x, node.y);
+          }}
         />
       </div>
     </div>
